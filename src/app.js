87188e84e1f93b445ee75158756222bd092a8dbf
Javier Montalvo <giojavi04@gmail.com>
2021-05-26 16:32:44 -0500

    cors

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const createError = require('http-errors');
 const logger = require('morgan');
+const cors = require('cors')
 
 const indexRouter = require('./routes/index');
 
@@ -9,6 +10,7 @@ const errorHandler = require('./middleware/errorHandler');
 const app = express();
 
 app.use(logger('dev'));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
